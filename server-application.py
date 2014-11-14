f46df59d5a0df97a99858a7ce8be6f14d021494c
julien-hadleyjack <github@jlhj.de>
2014-11-14 13:15:03 +0100

    no message

--- server-application.py
+++ server-application.py
@@ -1,12 +1,18 @@
+from datetime import date
+import json
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/meals/day')
+def meals_today():
+    url = "http://openmensa.org/api/v2/canteens/33/days/{0}/meals".format(date.today().isoformat())
+    page = requests.get(url)
+    meal_filter = lambda meal: {"name": meal["name"], "price": meal["prices"]["students"]}
+    return json.dumps(map(meal_filter, page.json()), indent=4, separators=(',', ': '))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
